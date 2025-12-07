3d3aa6004e8064045e0fca8324104592e622a4a8
MolleWare <hugo.molle@yahoo.fr>
2025-12-07 19:53:01 +0000

    Deploying to gh-pages from @ MolleWare/molleware.github.io@3db4774b51f9adc612bb7961aa4bfbcda83748a3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Zabardast/zabardast.github.io",
+    "data-repo": "MolleWare/molleware.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
